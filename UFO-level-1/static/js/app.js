462f171ca2ae61d4e8b6a79d033cfb6ca4b8f31b
Lorenz Mateo <mateo707@hawaii.edu>
2020-09-15 15:29:17 -0700

    add comments for to do's

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,28 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+// table body
+var tbody = d3.select("tbody");
+
+// filter button click
+var filterEntries = d3.select("#filter-btn");
+filterEntries.on("click", function() {
+
+    // code to prevent the page from refreshing
+
+    // get the key element: #datetime, #city, #state, #country, #shape, #comments
+
+    // get the value of the key elements
+
+    // create if statements to use for multiple filters
+
+    // create a refresh button
+
+
+});
+
+// create renderTable so that the table shows up
+
+// Create loop through all data to show everything in the filtered data
