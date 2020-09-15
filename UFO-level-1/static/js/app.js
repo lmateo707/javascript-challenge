b15d0e80e1f95076c449ca45107f8fe9dac07f2d
Lorenz Mateo <mateo707@hawaii.edu>
2020-09-15 16:17:22 -0700

    add the rest

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,12 +11,18 @@ var filterEntries = d3.select("#filter-btn");
 filterEntries.on("click", function() {
 
     // code to prevent the page from refreshing
+    d3.event.preventDefault();
 
     // get the key element: #datetime, #city, #state, #country, #shape, #comments
+    var datetimeKey = d3.select("#datetime");
 
     // get the value of the key elements
+    var datetimeValue = datetimeKey.property("value");
 
     // create if statements to use for multiple filters
+    if (datetimeValue != ""){
+        tableData = tableData.filter(entry => entry.datetime === datetimeValue);
+    }
 
     // create a refresh button
 
@@ -24,5 +30,20 @@ filterEntries.on("click", function() {
 });
 
 // create renderTable so that the table shows up
+renderTable();
 
 // Create loop through all data to show everything in the filtered data
+function renderTable(){
+    $("#tbodyid").empty();
+    console.log(tableData);
+    tableData.forEach(function(ufoSightings) {
+        console.log(ufoSightings);
+        var row = tbody.append("tr");
+        Object.entries(ufoSightings).forEach(function([key, value]) {
+            console.log(key,value);
+            var cell = tbody.append("td");
+            cell.text(value);
+
+        });
+    });
+}
