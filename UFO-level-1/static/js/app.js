992182a6825e022558eb015dbe89bfdcaaa147bc
Lorenz Mateo <mateo707@hawaii.edu>
2020-09-21 09:23:40 -0700

    work with tutor

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -15,16 +15,44 @@ filterEntries.on("click", function() {
 
     // get the key element: #datetime, #city, #state, #country, #shape, #comments
     var datetimeKey = d3.select("#datetime");
+    var cityKey = d3.select("#city");
+    var stateKey = d3.select("#state");
+    var countryKey = d3.select("#country");
+    var shapeKey = d3.select("#shape");
+    var commentsKey = d3.select("#comment");
 
     // get the value of the key elements
     var datetimeValue = datetimeKey.property("value");
+    var cityValue = cityKey.property("value").toLowerCase().trim();
+    var stateValue = stateKey.property("value").toLowerCase().trim();
+    var countryValue = countryKey.property("value").toLowerCase().trim();
+    var shapeValue = shapeKey.property("value").toLowerCase().trim();
+    var commentsValue = commentsKey.property("value").toLowerCase().trim();
+    
 
     // create if statements to use for multiple filters
     if (datetimeValue != ""){
         tableData = tableData.filter(entry => entry.datetime === datetimeValue);
     }
+    if (cityValue != ""){
+        tableData = tableData.filter(entry => entry.city === cityValue);
+    }
+    if (stateValue != ""){
+        tableData = tableData.filter(entry => entry.state === stateValue);
+    }
+    if (countryValue != ""){
+        tableData = tableData.filter(entry => entry.country === countryValue);
+    }
+    if (shapeValue != ""){
+        tableData = tableData.filter(entry => entry.shape === shapeValue);
+    }
+    if (commentsValue != ""){
+        tableData = tableData.filter(entry => entry.comment === commentsValue);
+    }
 
-    // create a refresh button
+    
+    console.log(tableData);
+    renderTable();
 
 
 });
@@ -34,7 +62,7 @@ renderTable();
 
 // Create loop through all data to show everything in the filtered data
 function renderTable(){
-    $("#tbodyid").empty();
+    
     console.log(tableData);
     tableData.forEach(function(ufoSightings) {
         console.log(ufoSightings);
